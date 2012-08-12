a64dbba19342d490bb9bd8620295338f8df85bdb
Sprocket <duncan@sprocket.co.nz>
2012-08-12 14:56:43 +1200

    Added study section
    And fundamentals page
    
    Signed-off-by: Sprocket <duncan@sprocket.co.nz>

---
+++ study/fundamentals.php
@@ -0,0 +1,63 @@
+<?php
+	require '../inc/functions.php';
+	echo page_header('SDV602 Study');
+?>
+<div class="container page-wrap">
+<div class="row span9 offset3">
+<h1>PHP Fundamentals</h1>
+<p>You will be expected to have an understanding of each of these key areas of PHP.</p>
+
+<h4>Setting a PHP script</h4>
+<ul>
+	<li>Incorporate PHP script into a web page</li>
+	<li>Include other php and html files into a script</li>
+</ul>
+
+<h4>Variables</h4>
+<ul>
+	<li>Setting and changing variables of each of the simple primitive types</li>
+</ul>
+
+<h4>Arrays</h4>
+<ul>
+	<li>Create simple, assocative and multi-dimensinal arrays</li>
+	<li>Add new items to an array</li>
+	<li>Select array items based on key and position</li>
+	<li>Perform an operation on each array item</li>
+</ul>
+
+
+<h4>Functions</h4>
+<ul>
+	<li>Creating and calling user-defined functions</li>
+	<li>Returning values from functions</li>
+	<li>Understand what <code>local scope</code> is</li>
+</ul>
+
+
+<h4>Controlling Application flow</h4>
+<ul>
+	<li>Using <code>if</code> and <code>switch</code> statements to perform tests and control the flow of your application</li>
+	<li>Use a location <code>header</code> to redirect browsers</li>
+</ul>
+
+<h4>Strings</h4>
+<ul>
+	<li>Joining two strings together</li>
+	<li>Using PHP variables in a string</li>
+	<li>Using <code>printf</code> function to format a string</li>
+</ul>
+
+<h4>Super globals</h4>
+<ul>
+	<li>Know what <code>$_GET</code> and <code>$_POST</code> super globals are and when you can use them. </li>
+</ul>
+
+<h4>Sessions</h4>
+<ul>
+	<li>Know how to set up and use <code>sessions</code></li>
+</ul>
+
+
+</div><!-- row -->
+</div><!-- page wrap -->
---
+++ study/index.php
@@ -0,0 +1,25 @@
+<?php
+	require '../inc/functions.php';
+	echo page_header('SDV602 Study');
+?>
+<div class="container page-wrap">
+<div class="row">
+
+<div class="span3">
+	<ul class="nav nav-tabs nav-stacked span3">
+		<li><a href="fundamentals.php">Fundamentals</a></li>
+	</ul>
+</div>		
+
+<div class="page-content span9">
+
+<h1>SDV Study Stuffs</h1>
+
+<h4><a href="fundamentals.php">PHP Fundamentals</a></h4>
+<p>A list of things you should know about PHP.</p>
+
+
+</div>
+
+</div><!-- row -->
+</div><!-- page-wrap -->
