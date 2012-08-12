f93f709e0cfb569111f045bfb8156851b4fad13f
Sprocket <duncan@sprocket.co.nz>
2012-08-12 18:31:33 +1200

    Updated list of fundamentals
    Signed-off-by: Sprocket <duncan@sprocket.co.nz>

--- study/fundamentals.php
+++ study/fundamentals.php
@@ -7,7 +7,7 @@
 <h1>PHP Fundamentals</h1>
 <p>You will be expected to have an understanding of each of these key areas of PHP.</p>
 
-<h4>Setting a PHP script</h4>
+<h4>Using PHP scripts</h4>
 <ul>
 	<li>Incorporate PHP script into a web page</li>
 	<li>Include other php and html files into a script</li>
@@ -15,23 +15,37 @@
 
 <h4>Variables</h4>
 <ul>
-	<li>Setting and changing variables of each of the simple primitive types</li>
+	<li>Set and change variables of each of the simple primitive types</li>
+	<li>Test that a variable exists and is of a certain type</li>
+	<li>Use global and local variables</li>
+</ul>
+
+<h4>Strings</h4>
+<ul>
+	<li>Concatenate multiple strings</li>
+	<li>Using PHP variables in a string</li>
+	<li>Using <code>printf</code> function to format variables into a string</li>
 </ul>
 
 <h4>Arrays</h4>
 <ul>
-	<li>Create simple, assocative and multi-dimensinal arrays</li>
+	<li>Create simple, assocative and multi-dimensional arrays</li>
 	<li>Add new items to an array</li>
 	<li>Select array items based on key and position</li>
 	<li>Perform an operation on each array item</li>
 </ul>
 
+<h4>Date and Time</h4>
+<ul>
+	<li>Create a string of the current date in a variety of formats</li>
+	<li>Make basic date calculations</li>
+</ul>
 
 <h4>Functions</h4>
 <ul>
-	<li>Creating and calling user-defined functions</li>
-	<li>Returning values from functions</li>
-	<li>Understand what <code>local scope</code> is</li>
+	<li>Create and call a user-defined function</li>
+	<li>Returning appropriate values from function</li>
+	<li>Have a basic understanding of <code>scope</code></li>
 </ul>
 
 
@@ -41,13 +55,6 @@
 	<li>Use a location <code>header</code> to redirect browsers</li>
 </ul>
 
-<h4>Strings</h4>
-<ul>
-	<li>Joining two strings together</li>
-	<li>Using PHP variables in a string</li>
-	<li>Using <code>printf</code> function to format a string</li>
-</ul>
-
 <h4>Super globals</h4>
 <ul>
 	<li>Know what <code>$_GET</code> and <code>$_POST</code> super globals are and when you can use them. </li>
@@ -55,9 +62,18 @@
 
 <h4>Sessions</h4>
 <ul>
-	<li>Know how to set up and use <code>sessions</code></li>
+	<li>Know how use <code>sessions</code> in an application</li>
+	<li>Create a cookie</li>
+	<li>Test for cookie and session variables</li>
 </ul>
 
+<h4>Files</h4>
+<ul>
+	<li>Iterate over a directory of files</li>
+	<li>Write a new line to a file</li>
+	<li>Overwrite a whole file</li>
+	<li>Read a file's contents</li>
+</ul>
 
 </div><!-- row -->
 </div><!-- page wrap -->
